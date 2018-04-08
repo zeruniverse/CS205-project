effb81938b0531b094621e4c72cdcd58c019ae09
Hongxiang Qiu <hongxiangqiu@gmail.com>
2018-04-08 18:41:10 -0400

    update acc

---
+++ DeepFlow_parallel/src_acc/acc.h
@@ -0,0 +1,9 @@
+//
+// Created by hongxiangqiu on 4/7/18.
+//
+
+#ifndef DEEPFLOW_ACC_H
+#define DEEPFLOW_ACC_H
+
+
+#endif
---
+++ DeepFlow_parallel/test_mpi_no_match.sh
@@ -0,0 +1,2 @@
+time ./cmake-build-debug/DeepFlow_mpi ./samples/1.ppm ./samples/2.ppm ./samples/my_output_mpi_no_match.flo
+../flow-code/color_flow ./samples/my_output_mpi_no_match.flo ./samples/my_output_mpi_no_match.png
