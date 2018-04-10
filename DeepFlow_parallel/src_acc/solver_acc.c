275bd30ea0c23ca6f8e7e4993d1c3d71ff65ca0e
Hongxiang Qiu <hongxiangqiu@gmail.com>
2018-04-10 00:44:41 -0400

    update

--- DeepFlow_parallel/src_acc/solver_acc.c
+++ DeepFlow_parallel/src_acc/solver_acc.c
@@ -102,7 +102,7 @@ void sor_coupled_acc(image_t *du, image_t *dv, const image_t *a11, const image_t
     const float *dpv = dpsis_vert->data;
 
 
-#pragma acc data copyin(dph[0:N], dpv[0:N], A11m[0:N], A12m[0:N], A22m[0:N], b1_data[0:N], b2_data[0:N], to_u[0:N], to_v[0:N]) copy(from_u, from_v)
+#pragma acc data copyin(dph[0:N], dpv[0:N], A11m[0:N], A12m[0:N], A22m[0:N], b1_data[0:N], b2_data[0:N], to_u[0:N], to_v[0:N]) copy(from_u[0:N], from_v[0:N])
     {
         for (int iter = 0; iter < iterations / 2; iter++) {
 #pragma acc parallel loop independent num_gangs(1) num_workers(1)
