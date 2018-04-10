8f75372bbbb218e047fb5912f2fe894cc87992c3
Hongxiang Qiu <hongxiangqiu@gmail.com>
2018-04-10 16:48:01 -0400

    update result

--- DeepFlow_parallel/test_ori.sh
+++ DeepFlow_parallel/test_ori.sh
@@ -1,2 +1,2 @@
-./cmake-build-debug/DeepFlow ./samples/1.ppm ./samples/2.ppm ./samples/my_output.flo -match ./samples/forward.match
+time ./cmake-build-debug/DeepFlow ./samples/1.ppm ./samples/2.ppm ./samples/my_output.flo -match ./samples/forward.match
 diff -b ./samples/my_output.flo ./samples/forward.flo
