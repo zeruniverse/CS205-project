028626fa433848db36eff18d9a08ee89cd5b8061
Hongxiang Qiu <hongxiangqiu@gmail.com>
2018-04-10 18:06:28 -0400

    add vanilla rbsor

--- DeepFlow_parallel/src_rbsor/solver_rbsor.c
+++ DeepFlow_parallel/src_rbsor/solver_rbsor.c
@@ -64,124 +64,86 @@ void calculate_constants(float *A11m, float *A12m, float *A22m,
     }
 }
 
+void rbsor_step(const int red,
+                float *du, float *dv,
+                const float omega,
+                const int W, const int H, const int stride,
+                const float *dph, const float *dpv,
+                const float *A11m, const float *A12m, const float *A22m,
+                const float *b1d, const float *b2d) {
+    for (int j = 0; j < H; j++) {
+        for (int i = (j % 2) + 1 - red; i < W; i += 2) {
+            float sigma_u, sigma_v, A11, A22, A12, B1, B2;
+            sigma_u = 0.0f;
+            sigma_v = 0.0f;
+            if (j > 0) {
+                sigma_u -= dpv[(j - 1) * stride + i] * du[(j - 1) * stride + i];
+                sigma_v -= dpv[(j - 1) * stride + i] * dv[(j - 1) * stride + i];
+            }
+            if (i > 0) {
+                sigma_u -= dph[j * stride + i - 1] * du[j * stride + i - 1];
+                sigma_v -= dph[j * stride + i - 1] * dv[j * stride + i - 1];
+            }
+            if (j < H - 1) {
+                sigma_u -= dpv[j * stride + i] * du[(j + 1) * stride + i];
+                sigma_v -= dpv[j * stride + i] * dv[(j + 1) * stride + i];
+            }
+            if (i < W - 1) {
+                sigma_u -= dph[j * stride + i] * du[j * stride + i + 1];
+                sigma_v -= dph[j * stride + i] * dv[j * stride + i + 1];
+            }
+            B1 = b1d[j * stride + i] - sigma_u;
+            B2 = b2d[j * stride + i] - sigma_v;
+
+            A11 = A11m[j * stride + i];
+            A12 = A12m[j * stride + i];
+            A22 = A22m[j * stride + i];
+
+            du[j * stride + i] =
+                    (1.0f - omega) * du[j * stride + i] + omega * (A22 * B1 - A12 * B2);
+            dv[j * stride + i] =
+                    (1.0f - omega) * dv[j * stride + i] + omega * (-A12 * B1 + A11 * B2);
+        }
+    }
+}
+
 void sor_coupled_rbsor(image_t *du, image_t *dv, const image_t *a11, const image_t *a12, const image_t *a22,
-                     const image_t *b1, const image_t *b2, const image_t *dpsis_horiz,
-                     const image_t *dpsis_vert, const int iterations, float omega) {
-    float *buffer_u = (float *) memalign(16, du->stride * du->height * sizeof(float));
-    float *buffer_v = (float *) memalign(16, dv->stride * dv->height * sizeof(float));
+                       const image_t *b1, const image_t *b2, const image_t *dpsis_horiz,
+                       const image_t *dpsis_vert, const int iterations, float omega) {
+    const int stride = du->stride;
+    const int H = du->height;
+    const int W = du->width;
+    const int N = stride * H;
 
-    float *from_u = du->data;
-    float *from_v = dv->data;
+    float *dud = du->data;
+    float *dvd = dv->data;
 
-    float *to_u = buffer_u;
-    float *to_v = buffer_v;
-
-    float *A11m = (float *) memalign(16, du->stride * du->height * sizeof(float));
-    float *A12m = (float *) memalign(16, du->stride * du->height * sizeof(float));
-    float *A22m = (float *) memalign(16, du->stride * du->height * sizeof(float));
+    float *A11m = (float *) memalign(16, N * sizeof(float));
+    float *A12m = (float *) memalign(16, N * sizeof(float));
+    float *A22m = (float *) memalign(16, N * sizeof(float));
 
     calculate_constants(A11m, A12m, A22m,
                         du, dv, a11, a12, a22, b1, b2, dpsis_horiz, dpsis_vert);
 
-    swap(&from_u, &to_u);
-    swap(&from_v, &to_v);
-
-    omega = 1.0;
+    const float *dpv = dpsis_vert->data;
+    const float *dph = dpsis_horiz->data;
+    const float *b1d = b1->data;
+    const float *b2d = b2->data;
 
     for (int iter = 0; iter < iterations; iter++) {
-        for (int p = 0; p <= 0; p++) {
-            swap(&from_u, &to_u);
-            swap(&from_v, &to_v);
-            for (int j = 0; j < du->height; j++) {
-                float sigma_u, sigma_v, A11, A22, A12, B1, B2;
-                for (int i = 0; i < du->width; i++) {
-                    sigma_u = 0.0f;
-                    sigma_v = 0.0f;
-                    if (j > 0) {
-                        sigma_u -= dpsis_vert->data[(j - 1) * du->stride + i] * from_u[(j - 1) * du->stride + i];
-                        sigma_v -= dpsis_vert->data[(j - 1) * du->stride + i] * from_v[(j - 1) * du->stride + i];
-                    }
-                    if (i > 0) {
-                        sigma_u -= dpsis_horiz->data[j * du->stride + i - 1] * from_u[j * du->stride + i - 1];
-                        sigma_v -= dpsis_horiz->data[j * du->stride + i - 1] * from_v[j * du->stride + i - 1];
-                    }
-                    if (j < du->height - 1) {
-                        sigma_u -= dpsis_vert->data[j * du->stride + i] * from_u[(j + 1) * du->stride + i];
-                        sigma_v -= dpsis_vert->data[j * du->stride + i] * from_v[(j + 1) * du->stride + i];
-                    }
-                    if (i < du->width - 1) {
-                        sigma_u -= dpsis_horiz->data[j * du->stride + i] * from_u[j * du->stride + i + 1];
-                        sigma_v -= dpsis_horiz->data[j * du->stride + i] * from_v[j * du->stride + i + 1];
-                    }
-                    B1 = b1->data[j * du->stride + i] - sigma_u;
-                    B2 = b2->data[j * du->stride + i] - sigma_v;
-
-                    A11 = A11m[j * du->stride + i];
-                    A12 = A12m[j * du->stride + i];
-                    A22 = A22m[j * du->stride + i];
-
-                    to_u[j * du->stride + i] =
-                            (1.0f - omega) * from_u[j * du->stride + i] + omega * (A22 * B1 - A12 * B2);
-                    to_v[j * du->stride + i] =
-                            (1.0f - omega) * from_v[j * du->stride + i] + omega * (-A12 * B1 + A11 * B2);
-                }
-            }
-        }
-    }
-
-    if (to_u != du->data) {
-        memcpy(du->data, to_u, du->stride * du->height * sizeof(float));
-        memcpy(dv->data, to_v, dv->stride * dv->height * sizeof(float));
+        rbsor_step(0,
+                   dud, dvd,
+                   omega,
+                   W, H, stride, dph, dpv,
+                   A11m, A12m, A22m, b1d, b2d);
+        rbsor_step(1,
+                   dud, dvd,
+                   omega,
+                   W, H, stride, dph, dpv,
+                   A11m, A12m, A22m, b1d, b2d);
     }
 
     free(A11m);
     free(A12m);
     free(A22m);
-    free(buffer_u);
-    free(buffer_v);
-}
-
-void sor_coupled_mpi_ori(image_t *du, image_t *dv, const image_t *a11, const image_t *a12, const image_t *a22,
-                         const image_t *b1, const image_t *b2, const image_t *dpsis_horiz,
-                         const image_t *dpsis_vert, const int iterations, const float omega) {
-    for (int iter = 0; iter < iterations; iter++) {
-        for (int j = 0; j < du->height; j++) {
-            float sigma_u, sigma_v, sum_dpsis, A11, A22, A12, B1, B2, det;
-            for (int i = 0; i < du->width; i++) {
-                sigma_u = 0.0f;
-                sigma_v = 0.0f;
-                sum_dpsis = 0.0f;
-                if (j > 0) {
-                    sigma_u -= dpsis_vert->data[(j - 1) * du->stride + i] * du->data[(j - 1) * du->stride + i];
-                    sigma_v -= dpsis_vert->data[(j - 1) * du->stride + i] * dv->data[(j - 1) * du->stride + i];
-                    sum_dpsis += dpsis_vert->data[(j - 1) * du->stride + i];
-                }
-                if (i > 0) {
-                    sigma_u -= dpsis_horiz->data[j * du->stride + i - 1] * du->data[j * du->stride + i - 1];
-                    sigma_v -= dpsis_horiz->data[j * du->stride + i - 1] * dv->data[j * du->stride + i - 1];
-                    sum_dpsis += dpsis_horiz->data[j * du->stride + i - 1];
-                }
-                if (j < du->height - 1) {
-                    sigma_u -= dpsis_vert->data[j * du->stride + i] * du->data[(j + 1) * du->stride + i];
-                    sigma_v -= dpsis_vert->data[j * du->stride + i] * dv->data[(j + 1) * du->stride + i];
-                    sum_dpsis += dpsis_vert->data[j * du->stride + i];
-                }
-                if (i < du->width - 1) {
-                    sigma_u -= dpsis_horiz->data[j * du->stride + i] * du->data[j * du->stride + i + 1];
-                    sigma_v -= dpsis_horiz->data[j * du->stride + i] * dv->data[j * du->stride + i + 1];
-                    sum_dpsis += dpsis_horiz->data[j * du->stride + i];
-                }
-                A11 = a11->data[j * du->stride + i] + sum_dpsis;
-                A12 = a12->data[j * du->stride + i];
-                A22 = a22->data[j * du->stride + i] + sum_dpsis;
-                det = A11 * A22 - A12 * A12;
-                B1 = b1->data[j * du->stride + i] - sigma_u;
-                B2 = b2->data[j * du->stride + i] - sigma_v;
-                du->data[j * du->stride + i] =
-                        (1.0f - omega) * du->data[j * du->stride + i] + omega * (A22 * B1 - A12 * B2) / det;
-                dv->data[j * du->stride + i] =
-                        (1.0f - omega) * dv->data[j * du->stride + i] + omega * (-A12 * B1 + A11 * B2) / det;
-            }
-        }
-    }
 }
