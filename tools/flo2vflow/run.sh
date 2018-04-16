7037698af2e945f806177393926243cf46b8a2f7
Jeffery Zhao <zzy8200@gmail.com>
2018-04-16 13:52:12 -0400

    Update run.sh

--- tools/flo2vflow/run.sh
+++ tools/flo2vflow/run.sh
@@ -6,8 +6,8 @@ while true; do
   file1="input/forward_${i}_${j}.flo"
   file2="input/backward_${j}_${i}.flo"
   if [ -a $file2 ]; then
-    eval $convertCommandLine "$file1" "output/ocv_${i}_${j}.vflo"
-    eval $convertCommandLine "$file2" "output/ocv_${j}_${i}.vflo"
+    eval $convertCommandLine "$file1" "output/ocv-forward-${i}-${j}.sVflow"
+    eval $convertCommandLine "$file2" "output/ocv-backward-${j}-${i}.sVflow"
     echo "Finish Processing flow between ${i} and ${j}"
   else
     break
