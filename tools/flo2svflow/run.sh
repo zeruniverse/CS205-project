e9df75a3aa236e16a948bfb4aef741596c92d8ea
Gedichter <shiyuhuang@g.harvard.edu>
2018-04-24 22:13:19 -0400

    update flow generator for slow motion

--- tools/flo2svflow/run.sh
+++ tools/flo2svflow/run.sh
@@ -1,13 +1,15 @@
 i=1
 j=2
-convertCommandLine="./converter"
+convertCommandLine="./generator"
 
 while true; do
   file1="input/forward_${i}_${j}.flo"
   file2="input/backward_${j}_${i}.flo"
   if [ -a $file2 ]; then
-    eval $convertCommandLine "$file1" "output/ocv-forward-${i}-${j}.sVflow"
-    eval $convertCommandLine "$file2" "output/ocv-backward-${j}-${i}.sVflow"
+    ii=$[$i - 1]
+    jj=$[$j - 1]
+    eval $convertCommandLine "$file1" "output/ocv-forward-$ii-$jj.sVflow"
+    eval $convertCommandLine "$file2" "output/ocv-backward-$jj-$ii.sVflow"
     echo "Finish Processing flow between ${i} and ${j}"
   else
     break
