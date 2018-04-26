c4634348f85a7134a8639cb6283dabbf93176c31
Jeffery Zhao <zzy8200@gmail.com>
2018-04-26 12:34:33 -0400

    Update compile_acc.sh

--- src/RBSOR_ACC/compile_acc.sh
+++ src/RBSOR_ACC/compile_acc.sh
@@ -1,4 +1,4 @@
 rm -rf build
 mkdir build
 cd build
-pgcc -acc -Minfo -Mlarge_arrays ../deepflow2_acc.c ../src_acc/*.c -lm -ljpeg -lpng -ta=tesla:cc50 -o deepflow2 
+pgcc -acc -Minfo -Mlarge_arrays -O3 -std=gnu11 ../deepflow2_acc.c ../src_acc/*.c -lm -ljpeg -lpng -ta=tesla:cc50 -o deepflow2 
