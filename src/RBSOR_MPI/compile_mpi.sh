8076a09001c769082a4c213c3a7ece7a8cead243
Jeffery Zhao <zzy8200@gmail.com>
2018-04-26 12:22:48 -0400

    Update compile_mpi.sh

--- src/RBSOR_MPI/compile_mpi.sh
+++ src/RBSOR_MPI/compile_mpi.sh
@@ -1,5 +1,5 @@
 rm -rf build
 mkdir build
 cd build
-mpicc ../deepflow2_mpi.c ../src/*.c -lm -ljpeg -lpng -o deepflow2 
+mpicc -O3 -std=gnu11 ../deepflow2_mpi.c ../src/*.c -lm -ljpeg -lpng -o deepflow2 
 cd ..
