1ad31f348ad36b75f8c0ed3cc21e334e4ca68a48
ZongrenZou <31862613+ZongrenZou@users.noreply.github.com>
2018-05-07 03:23:37 -0400

    Add files via upload

--- src/RBSOR_MPI/deepflow2_mpi.c
+++ src/RBSOR_MPI/deepflow2_mpi.c
@@ -54,7 +54,7 @@ int main(int argc, char ** argv){
     int rank,size;
     MPI_Comm_rank(MPI_COMM_WORLD,&rank);
     MPI_Comm_size(MPI_COMM_WORLD,&size);
-    //printf("%d,%d\n",rank,size);
+    printf("%d,%d\n",rank,size);
     double start,end;
     if (rank == 0)
 	start = MPI_Wtime();
